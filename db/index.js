65b1a12872f5456f501d10165d9398855a0c8966
Annah Patterson <annah.pelin@gmail.com>
2019-09-15 13:46:30 -0700

    Heroku fixes

--- db/index.js
+++ db/index.js
@@ -3,12 +3,13 @@ const mysql = require('mysql');
 let connectionString = process.env.DATABASE_URL;
 let dbUserName = process.env.DATABASE_USERNAME;
 let dbPassword = process.env.DATABASE_PASSWORD;
+let dbName = process.env.DATABASE_NAME;
 
 const db = mysql.createConnection({
   host: connectionString,
   username: dbUserName,
   password: dbPassword,
-  database: 'users'
+  database: dbName
 });
 
 db.connect();
