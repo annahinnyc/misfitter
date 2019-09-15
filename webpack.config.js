c6bd3570b4ac92715ed51112532f3dcba88c0450
Annah Patterson <annah.pelin@gmail.com>
2019-09-15 14:29:52 -0700

    Webpack adjustments

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+var SRC_DIR = path.join(__dirname, '/client/');
+var DIST_DIR = path.join(__dirname, '/public/');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test : /\.jsx?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html)$/,
+      threshold: 8192,
+      minRatio: 0.8
+    }),
+  ]
+};
