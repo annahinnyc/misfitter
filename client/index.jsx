2ab561ad2af7409966a891829a13b45e4b1bee5a
Annah Patterson <annah.pelin@gmail.com>
2019-09-19 11:38:41 -0700

    Adds Header & Footer

---
+++ client/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, browserHistory } from 'react-router-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<BrowserRouter history={browserHistory}><App /></BrowserRouter>, document.getElementById('app'));
