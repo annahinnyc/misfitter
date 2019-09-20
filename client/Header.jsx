f38b01906c1e3d72c5ddd3ac9450c087613b3110
Annah Patterson <annah.pelin@gmail.com>
2019-09-20 07:41:12 -0700

    Media assets

--- client/Header.jsx
+++ client/Header.jsx
@@ -22,9 +22,9 @@ class Header extends React.Component {
     return (
       <nav className="transparent">
         <div className="nav-wrapper">
-          <a href="/"><img className="logo" src="./assets/misfitter_icon.png" /></a>
+          <a href="/"><img className="logo" src="assets/misfitter_icon.png" /></a>
           <ul id="nav-mobile" className="right hide-on-small-only">
-            <a className="btn waves-effect black" href="/upload">Start Questionnaire</a>
+            <a className="btn waves-effect black" href="https://docs.google.com/forms/d/e/1FAIpQLSfvCyPqr7L3isnppbfcGbiDAjuqDEQdY8JQco1mPpFlvFrDqw/viewform">Start Questionnaire</a>
           </ul>
         </div>
       </nav>
